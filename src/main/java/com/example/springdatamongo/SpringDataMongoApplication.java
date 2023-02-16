03c00850565843fd8accb0d664c38119f06d8c84
pythagoras58 <solomondanso58@gmail.com>
2023-02-16 22:59:35 +0000

    Initial commit

---
+++ src/main/java/com/example/springdatamongo/SpringDataMongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdatamongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataMongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springdatamongo/SpringDataMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdatamongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
